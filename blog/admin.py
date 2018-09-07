86da74f8092c638da595ddf1bf6a2c94140e2cff
tt198408 <vedat.karakaplan@turktelekom.com>
2018-09-07 08:15:10 +0000

    Django uygulamam, ilk commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.a
+
+admin.site.register(Post)
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# blog/urls.py
+from django.urls import path
+
+from .views import homePageView
+
+urlpatterns = [
+    path('', homePageView, name='home')
+]
